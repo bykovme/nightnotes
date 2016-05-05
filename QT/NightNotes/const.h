9c872986abd0e4f016c185a8a3e0aa1fc7ef2e02
Alex Bykov <alex@bykov.me>
2016-05-05 12:33:41 +0300

    git tuning

--- QT/NightNotes/const.h
+++ QT/NightNotes/const.h
@@ -7,6 +7,6 @@
 
 #define MAX_FILE 262144
 #define DEFAULT_NEW_NAME "New Night Note"
-#define APP_VERSION_INTERNAL "5"
+#define APP_VERSION_INTERNAL "6"
 #define APP_VERSION_TEXT "1.0 BETA"
 #define NIGHT_NOTES "Night Notes"
